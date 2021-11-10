1d35cbedba00cb3580c4ef45172b5d8c6da734a6
rocklessg <abdulhafiz.suleiman@decagon.dev>
2021-11-10 03:36:40 +0100

    h1 tag

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello World')
+    res.send('<h1>hello World!</h1>');
 });
 
 app.listen(3000, () => {
